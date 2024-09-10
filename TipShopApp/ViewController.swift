cb37023b4f3c43f6e25515b647fa3f9d95c95ba4
DucLongDev <longvkook0312@gmail.com>
2024-09-10 17:23:06 +0700

    Initial Commit

---
+++ TipShopApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipShopApp
+//
+//  Created by Long, Nguyen Duc on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
